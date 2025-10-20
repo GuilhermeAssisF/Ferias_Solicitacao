309659297d3c3267fba9d5e8db7263f610463732
GuilhermeAssisF <assisguilhermefernandes@gmail.com>
2025-10-20 09:54:54 -0300

    add condicao data de pagamento

--- forms/5 - FLUIG-0109 - Solicitação de Férias/events/validateForm.js
+++ forms/5 - FLUIG-0109 - Solicitação de Férias/events/validateForm.js
@@ -1,4 +1,4 @@
-function validateForm(form){
+function validateForm(form) {
 
 	log.info("INICIO do VALIDATE do formulário FLUIG-0001 - FERIAS");
 
@@ -18,7 +18,7 @@ function validateForm(form){
 		}
 
 		if (form.getValue(aprovacao) == 2 && form.getValue(parecer) == '') {
-			Errors.push('Parecer não prenchido'); 
+			Errors.push('Parecer não prenchido');
 		}
 	}
 
@@ -34,15 +34,16 @@ function validateForm(form){
 		}
 	}
 
-	var getDtConvertida = function(campo) {
+	var getDtConvertida = function (campo) {
 		var dtArray = String(form.getValue(campo)).split('/');
 		return new Date(dtArray[2], dtArray[1] - 1, dtArray[0]);
 	};
 
-	if (atividade == 0 || atividade == 4 || atividade == 9  && (acaoUsuario=="true")) {
+	if (atividade == 0 || atividade == 4 || atividade == 9 && (acaoUsuario == "true")) {
 
 
-		
+
+
 		//NAO PERMITE ABRIR FERIAS PARA QUEM JA POSSUI FERIAS MARCADAS
 		var situacaoFerias = form.getValue('cpSituacaoFerias');
 		if (situacaoFerias == "Possuí férias marcadas") {
@@ -55,7 +56,7 @@ function validateForm(form){
 			var diasAbono = 0;
 			var diasDireito = parseInt(form.getValue('cpDiasDireito'));
 
-			if(!isEstagiario(funcao)){
+			if (!isEstagiario(funcao)) {
 				diasAbono = parseInt(form.getValue('cpDiasAbono'));
 			}
 
@@ -63,7 +64,7 @@ function validateForm(form){
 				Errors.push('Dias de Férias somados com Dias de Abono diferente de Dias de Direito.');
 			}*/
 
-			if(isEstagiario(funcao)){
+			if (isEstagiario(funcao)) {
 				if (diasFerias < 5) {
 					Errors.push('Não é permitido tirar menos do que 5 Dias de Férias.');
 				}
@@ -73,10 +74,10 @@ function validateForm(form){
 				}
 			}
 
-			if(isEstagiario(funcao)){
+			if (isEstagiario(funcao)) {
 				var dataFimFerias = getDtConvertida('cpDataFimFerias'),
-				dataFimContrato = getDtConvertida('cpFimContrato');
-				if (dataFimFerias > dataFimContrato){
+					dataFimContrato = getDtConvertida('cpFimContrato');
+				if (dataFimFerias > dataFimContrato) {
 					Errors.push('Não é permitido tirar Dias de Férias depois que o contrato terminou.');
 				}
 			}
@@ -84,12 +85,12 @@ function validateForm(form){
 			//validade idade e abono
 			var abono = form.getValue('cpHaveraAbono');
 			var idade = parseInt(form.getValue('cpIdade'));
-		/*	if(abono=="1" && (idade>parseInt(50) || idade<parseInt(18))){
-				Errors.push('O abono de férias não pode ser utilizado, por colaboradores menores de 18 anos e maiores de 50 anos.');
-			}*/
+			/*	if(abono=="1" && (idade>parseInt(50) || idade<parseInt(18))){
+					Errors.push('O abono de férias não pode ser utilizado, por colaboradores menores de 18 anos e maiores de 50 anos.');
+				}*/
 			var cpJaTemFerias = parseInt(form.getValue('cpJaTemFerias'));
-			var vazio ="";
-			if(cpJaTemFerias=="1"){
+			var vazio = "";
+			if (cpJaTemFerias == "1") {
 				Errors.push('Já existe chamado de férias aberto, para este colaborador.');
 			}
 
@@ -101,50 +102,50 @@ function validateForm(form){
 
 			//coloca data em formato brasileiro
 
-			var Dtnac= new Date(cpfimfperAquiData);
+			var Dtnac = new Date(cpfimfperAquiData);
 			var diaDataNac = Dtnac.getDate();
 			var mesDataNac = Dtnac.getMonth();
 			mesDataNac = mesDataNac;
 			var anoDataNac = Dtnac.getFullYear();
 
-			if(diaDataNac<10){
-				diaDataNac= "0"+ diaDataNac;
-			}if(mesDataNac<10){
-				mesDataNac= "0"+ mesDataNac;
+			if (diaDataNac < 10) {
+				diaDataNac = "0" + diaDataNac;
+			} if (mesDataNac < 10) {
+				mesDataNac = "0" + mesDataNac;
 			}
-			
+
 			var faltas = form.getValue('cpNumeroFaltas');
-			
-			var datafimNac = new Date(mesDataNac+"/"+diaDataNac+"/"+anoDataNac);
-			
-			if(parseFloat(faltas)>parseFloat(0)){
-				datafimNac.setDate(datafimNac.getDate() + parseFloat(1)+parseFloat(faltas));
-			}else{
+
+			var datafimNac = new Date(mesDataNac + "/" + diaDataNac + "/" + anoDataNac);
+
+			if (parseFloat(faltas) > parseFloat(0)) {
+				datafimNac.setDate(datafimNac.getDate() + parseFloat(1) + parseFloat(faltas));
+			} else {
 				datafimNac.getDate();
 			}
 			var mesprox = datafimNac.getMonth();
 			var diaprox = datafimNac.getDate();
-			mesprox = parseFloat(mesprox)+parseFloat(1);
-			
-			if(mesprox<10){
-				mesprox= "0"+ mesprox;
-			}if(diaprox<10){
-				diaprox= "0"+ diaprox;
-			}
-			
-			var DataFinaldoprox =diaprox +"/"+mesprox+"/"+datafimNac.getFullYear();
-			//data final do periodo de ferias 
-			
-			
-			var Data = FimFerias;
-			var diaData = Data.substring(0,2);
-			var mesData = Data.substring(3,5);
-			var anoData = Data.substring(6,10);
+			mesprox = parseFloat(mesprox) + parseFloat(1);
 
-			
-			var datafim = new Date(mesData+"/"+diaData+"/"+anoData);
-			
-		    /*if(new Date(datafim)>new Date(cpfimfperAquiData)){
+			if (mesprox < 10) {
+				mesprox = "0" + mesprox;
+			} if (diaprox < 10) {
+				diaprox = "0" + diaprox;
+			}
+
+			var DataFinaldoprox = diaprox + "/" + mesprox + "/" + datafimNac.getFullYear();
+			//data final do periodo de ferias 
+
+
+			var Data = FimFerias;
+			var diaData = Data.substring(0, 2);
+			var mesData = Data.substring(3, 5);
+			var anoData = Data.substring(6, 10);
+
+
+			var datafim = new Date(mesData + "/" + diaData + "/" + anoData);
+
+			/*if(new Date(datafim)>new Date(cpfimfperAquiData)){
 				Errors.push('A data final das férias, não pode ser superior ao Início do próximo período aquisitivo '+ DataFinaldoprox);
 			}	*/
 
@@ -156,13 +157,52 @@ function validateForm(form){
 			validaVazio('cpDataAdmissao', 'Data de admissão não informada');
 			validaVazio('cpDtPagto', 'Data de Pagamento não informada');
 
+			// ############### INÍCIO DO BLOCO DE CÓDIGO ADICIONADO ###############
+			if (form.getValue('cpDataInicioFerias') != '' && form.getValue('cpDtPagto') != '') {
+				try {
+					// Função auxiliar para converter dd/MM/yyyy para Date
+					var parseDate = function (dateStr) {
+						var parts = dateStr.split('/');
+						// new Date(year, monthIndex, day) - monthIndex é 0-based
+						return new Date(parseInt(parts[2], 10), parseInt(parts[1], 10) - 1, parseInt(parts[0], 10));
+					};
 
-		/*	var solicitante = form.getValue('cpMatriculaSolicitante'),
-			diretor = form.getValue('cpDiretor');
+					var dataInicioFeriasStr = form.getValue('cpDataInicioFerias');
+					var dataPagamentoStr = form.getValue('cpDtPagto');
 
-			if (solicitante != diretor) {
-				validaVazio('cpGestorImediato', 'Gestor imediato não informado');
-			}*/
+					var dataInicioFerias = parseDate(dataInicioFeriasStr);
+					var dataPagamento = parseDate(dataPagamentoStr);
+
+					// Calcula a data limite para pagamento (2 dias antes do início das férias)
+					var dataLimitePagamento = new Date(dataInicioFerias);
+					dataLimitePagamento.setDate(dataInicioFerias.getDate() - 2);
+
+					// Define as horas, minutos, segundos e milissegundos para 0 para comparar apenas as datas
+					dataPagamento.setHours(0, 0, 0, 0);
+					dataLimitePagamento.setHours(0, 0, 0, 0);
+					dataInicioFerias.setHours(0, 0, 0, 0);
+
+
+					// Valida se a data de pagamento é *anterior* à data limite (mais de 2 dias antes)
+					// ou se a data de pagamento é *posterior* à data de início das férias
+					if (dataPagamento < dataLimitePagamento || dataPagamento > dataInicioFerias) {
+						Errors.push('A data de pagamento deve ocorrer no máximo 2 dias antes do início das férias e não pode ser posterior à data de início.');
+					}
+
+				} catch (e) {
+					log.error("Erro ao validar datas de pagamento/início de férias: " + e);
+					Errors.push("Erro ao processar as datas. Verifique os formatos (dd/MM/yyyy).");
+				}
+			}
+			// ############### FIM DO BLOCO DE CÓDIGO ADICIONADO ###############
+
+
+			/*	var solicitante = form.getValue('cpMatriculaSolicitante'),
+				diretor = form.getValue('cpDiretor');
+	
+				if (solicitante != diretor) {
+					validaVazio('cpGestorImediato', 'Gestor imediato não informado');
+				}*/
 
 			//dados do periodo aquisitivo
 			validaVazio('cpSituacaoFerias', 'Situação das férias não informada');
@@ -171,14 +211,14 @@ function validateForm(form){
 			validaVazio('cpNumeroFaltas', 'Número de faltas não informado');
 			//dados das ferias
 			validaVazio('cpDiasDireito', 'Dias de direito não informado');
-			if(!isEstagiario(funcao)){
+			if (!isEstagiario(funcao)) {
 				validaVazio('cpHaveraAbono', 'Haverá abono não informado');
 				validaVazio('cpDiasAbono', 'Dia de abono não informado');
 				validaVazio('cpAntecipar13Salario', 'Antecipar o pagamento da 1ª parcela do 13º salário não informado');
 				var antecipar13Salario = form.getValue('cpAntecipar13Salario');
 				var selecionouImpressao = form.getValue('cp13SalarioImprimir');
-				if(antecipar13Salario == 1){
-					if(selecionouImpressao == 'N') {
+				if (antecipar13Salario == 1) {
+					if (selecionouImpressao == 'N') {
 						Errors.push('É necessário imprimir e assinar o Termo de Adiantamento do 13º salário');
 					}
 				}
@@ -199,32 +239,32 @@ function validateForm(form){
 			}
 
 		}
-		
-	/*	validaVazio('Ckb1V','Aviso de Férias.');
-		validaVazio('Ckb2V','Recibo de Férias.')*/
 
-		 
-		
-	} 
-	if (atividade == 14  && (acaoUsuario=="true")) { //GESTOR IMEDIATO
+		/*	validaVazio('Ckb1V','Aviso de Férias.');
+			validaVazio('Ckb2V','Recibo de Férias.')*/
+
+
+
+	}
+	if (atividade == 14 && (acaoUsuario == "true")) { //GESTOR IMEDIATO
 		validaAprovacao('cpAprovarGestor', 'cpParecerGestor');
 
-	} 
-	if ( atividade==20 && (acaoUsuario=="true")) { //GESTOR IMEDIATO EM ATRASO
+	}
+	if (atividade == 20 && (acaoUsuario == "true")) { //GESTOR IMEDIATO EM ATRASO
 		validaAprovacao('cpAprovarGestor2', 'cpParecerGestor2');
 
-	} 
-	if ((atividade==153 || atividade==136) && (acaoUsuario=="true")) { //VALIDAR KIT DE FÉRIAS
+	}
+	if ((atividade == 153 || atividade == 136) && (acaoUsuario == "true")) { //VALIDAR KIT DE FÉRIAS
 		validaAprovacao('cpAprovarGestor3', 'cpParecerGestor3');
 
 	}
-	
-	if ( atividade==9 && (acaoUsuario=="true")) { //reabertura
+
+	if (atividade == 9 && (acaoUsuario == "true")) { //reabertura
 		validaAprovacao('cpAprovacaoSolicitante', 'cpParecerAprovacaoSolicitante');
 
 	}
-	
-	
+
+
 
 	if (Errors.length) {
 		throw Errors[0];
